731f0021dd97ed509c3a83bc2f3d98abd2dbbe97
pipo102 <dvelenik@gmail.com>
2020-05-28 16:49:24 +0200

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -1,24 +1,92 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 11 16:03:12 2020
+Created on Thu May 28 14:41:41 2020
 
 @author: davidv
 """
 
-""" čisti listu po n kriteriju i vraća istu listu ! napraviti kao list comprehension
 
-a = ["md",3,4,23,"kd",34,"odo","dkk",7]
-print(a)
-
-def cisti_listu(lista):
-    brojac = 0
-    for el in lista:
-        if isinstance(el, str) == False:
-            lista[brojac] = el
-            brojac += 1
-    del lista[brojac:]
-
-cisti_listu(a)
-print(a)
 """
+[x] initial state of char, item => keys ...dict
+[x] values => number of items, empty key, default coin : 10
+[x] adding item to dict
+[x] list spec item in inventory and quantity
+[x] print all inventory
+[x] removing item from inventory
+[] adding several items at once
+
+"""
+
+invent = dict()
+
+
+def menu():
+    izb = input("Za ispis cijelog inv 'i' za ispis pojedinog pred 'p', dodavanje 'd', micanje 'r', izlaz 'q': ")
+    while izb != "q":
+        if izb in opcije:
+            nar = opcije[izb]
+            nar()
+        else:
+            print("nepoznata naredba!")
+        
+        izb = input("Za ispis cijelog inv 'i' za ispis pojedinog pred 'p', dodavanje 'd', micanje 'r', izlaz 'q': ")
+    
+    if izb == "q": print("Kraj!")
+
+
+def ispis():
+    for k,v in invent.items():
+        print(f"predmet {k} => količina {v}")
+
+
+def ispisp():
+    k = input("koji predmet trzis?").lower()
+    print(f"predmet {k} ima {invent[k]} u inventoriju.")
+
+
+def dodavanje():
+    predmet = [input("sta zelis dodati?").lower()]
+     
+    
+    if predmet == [""]:
+        ubaci()
+    else:
+        for i in predmet:
+            try:
+                kolicina = int(input("količina:"))
+            except ValueError:
+                kolicina = 0
+                
+            ubaci(i, kolicina)
+    
+    
+def ubaci(predmet="coin", kolicina=10):
+    if predmet in invent: 
+        invent[predmet] += kolicina
+    else:
+        invent[predmet] = kolicina
+
+   
+def micanje():
+    k = input("sta želiš izbrisati?:").lower()
+
+    if k in invent: 
+        v = int(input("za koliko?"))
+        if invent[k] - v <= 0:
+            invent[k] = 0
+        else:
+            invent[k] -= v
+    else:
+        print("Predmet nije u inventoriju!")
+
+
+opcije = {
+    "i" : ispis,
+    "p" : ispisp,
+    "d" : dodavanje,
+    "r" : micanje   
+    }
+
+menu()
+print(invent)
