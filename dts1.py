f25568ceaca6b5afa1b3b92370d8170c75b50b36
pipo102 <dvelenik@gmail.com>
2020-06-04 14:21:35 +0200

    Update dts1.py

--- dts1.py
+++ dts1.py
@@ -5,6 +5,7 @@ Created on Thu May 28 11:57:55 2020
 
 @author: davidv
 """
+import pprint
 
 users = [
        { "id": 0, "name": "Hero" },
@@ -22,13 +23,41 @@ users = [
 
 friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
 for user in users:
-    user["friend"] = []
+    user["friends"] = []
 
-for x,y in friendships:
-    users[x]["friend"].append(users[y])
-    users[y]["friend"].append(users[x])
+def dodaj(x,y):
+    """
+
+    Parameters
+    ----------
+    arg : list
+        Takes tuples with friend connections and add them accordingly to friends.
+
+    Returns
+    -------
+    None.
+
+    """
+    for user in users:
+        if user["id"] == x: 
+            user["friends"].append(y)
+        elif user["id"] == y:
+            user["friends"].append(x)
+
+def av_connections():
+    total_connections = sum(len(user["friends"]) for user in users)
+    average_connections = total_connections / len(users)
+    return average_connections    
+
     
-print(users)
+for connection in friendships:
+    x,y = connection
+    dodaj(x,y)
 
-print(friendships)
+pp = pprint.PrettyPrinter()
+
+pp.pprint(users)        
+
+pp.pprint(av_connections())
